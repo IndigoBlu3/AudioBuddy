8e1405fc3e4568b43f8fc7d86b15f6d956db47ea
IndigoBlu3 <SJDre@web.de>
2021-08-06 18:28:21 +0200

    Update AudioBuddy.cs

--- Runtime/AudioBuddy.cs
+++ Runtime/AudioBuddy.cs
@@ -2,10 +2,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using UnityEngine;
 #if UNITY_EDITOR
 using UnityEditor;
 #endif
-using UnityEngine;
+
 
 namespace AudioBuddyTool
 {
